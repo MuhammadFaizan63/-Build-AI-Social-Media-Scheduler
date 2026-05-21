66155bffc0f21e96fc1401ccf88c59d9f5139fdd
smartfaizan63-pixel <smartfaizan63@gmail.com>
2026-05-21 18:14:22 +0500

    auth complete hogya agy kam chl rha ha

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,24 @@
 "use client"
 
 import { LeafIcon } from "lucide-react";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
+import { Span } from "next/dist/trace";
 
 interface LogoProps {
-    name?: stirng;
+    name?: string;
     className?: string;
     hideName?: boolean
 }
+
+const Logo = ({ name = "Lemon.ai", className, hideName = false }: LogoProps) => {
+    return (
+        <div className={cn("flex items-center gap-2,className")}>
+            <div className="flex  h-6 items-center justify-center rounded-lg bg-primary text-primary text-primary-foreground">
+                <LeafIcon className="h-4 w-4"/>
+            </div>
+            {!hideName && <span className="text-lg font-bold">{name}</span>}
+        </div>
+    )
+}
+
+export default Logo;
